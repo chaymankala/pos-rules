1335de59496e2e9653a95c2f9a2638a573bd8310
chaymankala <chaithanya.mankala@gmail.com>
2019-07-24 18:16:18 +0530

    [update] exporting functions implemented

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ var cart = [];
  * Provide set of rules to initialise this 
  *
  * @param {array} _ pass array of rules
+ * @throws {error} if the argument is not an array
  */
 function loadRules(_) {
     if (!_ || (_ && !_.length)) {
@@ -12,6 +13,7 @@ function loadRules(_) {
     }
     else {
         _rules = _;
+        return;
     }
 };
 
@@ -27,9 +29,14 @@ function clearRules() {
  * Add items to cart
  *
  * @param {object} item pass item id and its quantity
+ * @returns {Number} Cart length after adding
  */
-function add(item) {
-    cart.push(item);
+function add({ id, quantity, unit_cost }) {
+    if ((!id && id != 0) || !quantity || (!unit_cost && unit_cost != 0)) {
+        throw "E102 - Invalid argument";
+    }
+    cart.push({ id, quantity, unit_cost });
+    return cart.length;
 }
 
 
@@ -37,12 +44,39 @@ function add(item) {
  * remove items from cart
  *
  * @param {Number} index pass item index to remove
+ * @returns {Number} Cart length after removing
  */
 function remove(index) {
     cart.splice(index, 1);
+    return cart.length;
 }
 
 
+/**
+ * remove all items from cart
+ *
+ */
+function clearCart() {
+    cart = [];
+}
+
+
+/**
+ * find total value of the cart
+ *
+ */
+function total(ignore_rules = false) {
+    let total = 0;
+
+    const __ = ignore_rules === true ? cart : validateCart();
+
+    for (let i of __) {
+        total += i.unit_cost;
+    }
+
+
+    return total;
+}
 
 
 export default {
