cc75592b6111d8330bb431052d2a830e9a9d0c04
chaymankala <chaithanya.mankala@gmail.com>
2019-07-24 18:48:09 +0530

    [update] rule engine beta

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 var _rules = [];
 var cart = [];
+const RULE_TYPES = {
+    DISCOUNT: "DISCOUNT",
+    GET_X_FOR_Y: "GET_X_FOR_Y"
+}
 
 /**
  * Provide set of rules to initialise this 
@@ -31,11 +35,11 @@ function clearRules() {
  * @param {object} item pass item id and its quantity
  * @returns {Number} Cart length after adding
  */
-function add({ id, quantity, unit_cost }) {
-    if ((!id && id != 0) || !quantity || (!unit_cost && unit_cost != 0)) {
+function add({ id, unit_cost }) {
+    if ((!id && id != 0) || (!unit_cost && unit_cost != 0)) {
         throw "E102 - Invalid argument";
     }
-    cart.push({ id, quantity, unit_cost });
+    cart.push({ id, unit_cost });
     return cart.length;
 }
 
@@ -64,18 +68,81 @@ function clearCart() {
 /**
  * find total value of the cart
  *
+ * @param {string} customer_id Customer ID to validate rules
  */
-function total(ignore_rules = false) {
+function total(customer_id, ignore_rules = false) {
     let total = 0;
 
-    const __ = ignore_rules === true ? cart : validateCart();
+    ignore_rules === true ? null : validateCart(customer_id);
 
-    for (let i of __) {
+    for (let i of cart) {
         total += i.unit_cost;
     }
 
 
     return total;
+}
+
+function validateCart(customer_id) {
+    if ((!cart || (cart && !cart.length) || (!_rules || (_rules && !_rules.length)))) {
+        return [];
+    }
+
+    let _rule = _rules.find(___ => ___ && ___.customer_id === customer_id);
+
+    if (_rule && _rule.rules && _rule.rules.length) {
+        let cRules = _rule.rules;
+        for (let i of cRules) {
+            applyRule(i);
+        }
+    }
+    else {
+        return [];
+    }
+
+}
+
+function applyRule(rule) {
+    if (rule && rule.type && rule.product_id) {
+        switch (rule.type) {
+            case RULE_TYPES.DISCOUNT: {
+                if (rule.discount && !isNaN(rule.discount) && rule.discount > 0) {
+                    for (let i of cart) {
+                        if (rule.product_id == i.id) {
+                            i.unit_cost = i.unit_cost - (i.unit_cost * (rule.discount / 100));
+                        }
+                    }
+                }
+                break;
+            }
+            case RULE_TYPES.GET_X_FOR_Y: {
+                if (!isNaN(rule.x) && !isNaN(rule.y) && rule.x > 0 && rule.y > 0 && x > y) {
+                    let ys = 0;
+                    let residue = x - y;
+                    let residue2 = 0;
+                    for (let i of cart) {
+                        if (rule.product_id == i.id) {
+                            if (residue2 == 0) {
+                                ys++;
+                            }
+                            else {
+                                residue2--;
+                                rule.unit_cost = 0;
+                            }
+                        }
+                        if (ys == y) {
+                            residue2 = residue;
+                        }
+
+                    }
+                }
+                break;
+            }
+            default:
+                return;
+        }
+    }
+
 }
 
 
