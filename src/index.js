3c9fd2445ed463a4521be6791a57adffd3f291d2
chaymankala <chaithanya.mankala@gmail.com>
2019-07-24 18:05:51 +0530

    [update] elem function definitions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 var _rules = [];
+var cart = [];
 
+/**
+ * Provide set of rules to initialise this 
+ *
+ * @param {array} _ pass array of rules
+ */
 function loadRules(_) {
     if (!_ || (_ && !_.length)) {
         throw "E101 - Please pass valid rules";
@@ -9,8 +15,31 @@ function loadRules(_) {
     }
 };
 
-function clearRules(){
+/**
+ * Clear all Rules
+ *
+ */
+function clearRules() {
     _rules = [];
+}
+
+/**
+ * Add items to cart
+ *
+ * @param {object} item pass item id and its quantity
+ */
+function add(item) {
+    cart.push(item);
+}
+
+
+/**
+ * remove items from cart
+ *
+ * @param {Number} index pass item index to remove
+ */
+function remove(index) {
+    cart.splice(index, 1);
 }
 
 
